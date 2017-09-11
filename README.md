efdb4d2f16d7fb7a400f760b379dc50adecde8ad
Asiazdx <2081048066@qq.com>
2017-09-11 14:29:13 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # vue-music-compile
 vue音乐App编译打包后的文件，下载下来可直接在node环境下运行
+安装步骤：
+ **1.将源码clone到本地**   
+ **2.进入项目目录**    
+ **3.在cmd中执行npm install（或者使用淘宝镜像cnpm install）下载依赖包中的模块**    
+ **4.运行sever.js文件，执行node server.js**  
+ **5.在本地浏览器中运行localhost:18080/**  
---
+++ server.js
@@ -0,0 +1,56 @@
+var express=require('express')
+var compression = require('compression')
+var axios=require('axios')
+
+
+var app = express()
+
+var apiRoutes = express.Router()
+
+apiRoutes.get('/getDiscList', function (req, res) {
+  var url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then(function(response){
+    res.json(response.data)
+  }).catch(function(e){
+    console.log(e)
+  })
+})
+
+apiRoutes.get('/lyric', function (req, res) {
+  var url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg'
+
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then(function(response){
+    var ret = response.data
+    if (typeof ret === 'string') {
+      var reg = /^\w+\(({[^\(\)]+})\)$/
+      var matches = response.data.match(reg)
+      if (matches) {
+        ret = JSON.parse(matches[1])
+      }
+    }
+    res.json(ret)
+
+  }).catch(function(e){
+    console.log(e)
+  })
+})
+
+app.use('/api', apiRoutes)
+
+app.use(compression())
+
+app.use(express.static('./dist'))
+
+app.listen(18080)
